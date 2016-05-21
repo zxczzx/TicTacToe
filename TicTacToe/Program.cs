a282bcace7e631a94b41e601a3a126e515f70df1
zxczzxPC <zxczzx.malek@gmail.com>
2016-05-21 18:47:44 +0200

    Creation of Game GUI and server projects. Adding SFML libs

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SFML;
+using SFML.Graphics;
+
+namespace SFML.TicTacToe
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Testing SFML framework
+            RenderWindow window = new RenderWindow(new Window.VideoMode(200, 200), "test");
+            CircleShape cs = new CircleShape(100.0f);
+            cs.FillColor = Color.Green;
+            window.SetActive();
+            while (window.IsOpen)
+            {
+                window.Clear();
+                window.DispatchEvents();
+                window.Draw(cs);
+                window.Display();
+            }
+        }
+    }
+}
